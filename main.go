5a6b9ea11a86ce51c0d879c2d5b894d0f0b6b6e0
mhebant <27782451+mhebant@users.noreply.github.com>
2021-01-19 20:30:24 +0100

    Add basic http handler

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 	"user-agent-api/useragent"
+	"net/http"
 )
 
 func main() {
 	repo, _ := useragent.RepositoryFromFile("user-agents.json")
-	fmt.Println(repo.Query("Overcast ( http://overcast.fm/; Apple Watch podcast app)"))
-	fmt.Println(repo.Query("Overcast/1.0 Podcast Sync"))
-	fmt.Println(repo.Query("de.danoeh.antennapod/1.7.3b (Linux;Android 8.0.0) ExoPlayerLib/2.9.3"))
+	
+	http.HandleFunc("/info", func(resp http.ResponseWriter, req *http.Request) {
+		if req.Method != "GET" {
+			resp.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprint(resp, "only GET method is suported")
+			return
+		}
+
+		ua := req.URL.Query().Get("ua")
+		if ua == "" {
+			resp.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(resp, "query parameter required")
+			return
+		}
+
+		uaInfo := repo.Query(ua)
+		resp.WriteHeader(http.StatusOK)
+		fmt.Fprint(resp, uaInfo)
+	})
+	http.ListenAndServe(":8080", nil)
 }
